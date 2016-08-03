4814f06562f5871364d9c588a18cc5ced1a5dd9e
Surma <surma@surma.link>
2016-08-03 17:25:00 +0100

    Generate full table

--- build.js
+++ build.js
@@ -2,16 +2,6 @@ const Handlebars = require('handlebars');
 const fs = require('fs');
 
 const data = require('./data.json');
-
-Handlebars.registerHelper('status', function(api, browser, field) {
-  try {
-    return data.status[api][browser][field];
-  } catch(e) {
-    console.log(`No data for ${api}/${browser}/${field}`);
-    return '';
-  }
-});
-
 const rawTpl = fs.readFileSync('index.hbs', 'utf-8');
 const tpl = Handlebars.compile(rawTpl);
 fs.writeFileSync('index.html', tpl(data), 'utf-8');
