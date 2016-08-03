9087d835c63c18027926d7d6753395a88e6242d4
Surma <surma@surma.link>
2016-08-03 23:08:30 +0100

    Add partials to build

--- build.js
+++ build.js
@@ -1,6 +1,15 @@
 const Handlebars = require('handlebars');
 const fs = require('fs');
 
+Handlebars.registerHelper('read_partial', (api, browser) => {
+  try {
+    return fs.readFileSync(`partials/${api}_${browser}.html`, 'utf-8');
+  } catch (e) {
+    console.log(`${api}_${browser}: ${e}`);
+    return '';
+  }
+});
+
 const data = require('./data.json');
 const rawTpl = fs.readFileSync('index.hbs', 'utf-8');
 const tpl = Handlebars.compile(rawTpl);
